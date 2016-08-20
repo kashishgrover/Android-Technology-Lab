fd44d7237f888590b77bb8a8f131200a59daebac
Kashish Grover <kashishgrover95@gmail.com>
2016-08-20 09:47:30 +0530

    asdf

---
+++ Lab2/Arnav/Lab2Constants.java
@@ -0,0 +1,9 @@
+package com.mit.project2;
+
+/**
+ * Created by Arnav on 13-Aug-16.
+ */
+public class Lab2Constants {
+    public static String username = "arnav";
+    public static String password = "hello";
+}
