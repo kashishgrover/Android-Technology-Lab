7e934474e16f1ef20f1d3ac14a7de4533a0cd664
asdf <asdf>
2016-09-24 09:31:04 +0530

    Lab7 Program Ready

--- Lab7/app/src/main/java/com/example/student/lab7/MainActivity.java
+++ Lab7/app/src/main/java/com/example/student/lab7/MainActivity.java
@@ -1,37 +1,202 @@
 package com.example.student.lab7;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button but0, but1, but2, but3, but4, but5, but6,
+            but7, but8, but9, butAdd, butSub, butDivision,
+            butMul, but10, butC, butEqual;
+
+    EditText edt1;
+
+    float mValueOne, mValueTwo;
+
+    boolean mAddition, mSubtract, mMultiplication, mDivision;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        but0 = (Button) findViewById(R.id.but0);
+        but1 = (Button) findViewById(R.id.but1);
+        but2 = (Button) findViewById(R.id.but2);
+        but3 = (Button) findViewById(R.id.but3);
+        but4 = (Button) findViewById(R.id.but4);
+        but5 = (Button) findViewById(R.id.but5);
+        but6 = (Button) findViewById(R.id.but6);
+        but7 = (Button) findViewById(R.id.but7);
+        but8 = (Button) findViewById(R.id.but8);
+        but9 = (Button) findViewById(R.id.but9);
+        but10 = (Button) findViewById(R.id.but10);
+        butAdd = (Button) findViewById(R.id.butadd);
+        butSub = (Button) findViewById(R.id.butsub);
+        butMul = (Button) findViewById(R.id.butmul);
+        butDivision = (Button) findViewById(R.id.butdiv);
+        butC = (Button) findViewById(R.id.butC);
+        butEqual = (Button) findViewById(R.id.buteql);
+        edt1 = (EditText) findViewById(R.id.edt1);
+
+
+        but1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edt1.setText(edt1.getText() + "1");
+            }
+        });
+
+        but2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edt1.setText(edt1.getText() + "2");
+            }
+        });
+
+        but3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edt1.setText(edt1.getText() + "3");
+            }
+        });
+
+        but4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edt1.setText(edt1.getText() + "4");
+            }
+        });
+
+        but5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edt1.setText(edt1.getText() + "5");
+            }
+        });
+
+        but6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edt1.setText(edt1.getText() + "6");
+            }
+        });
+
+        but7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edt1.setText(edt1.getText() + "7");
+            }
+        });
+
+        but8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edt1.setText(edt1.getText() + "8");
+            }
+        });
+
+        but9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edt1.setText(edt1.getText() + "9");
+            }
+        });
+
+        but0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edt1.setText(edt1.getText() + "0");
+            }
+        });
+
+        butAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (edt1 == null) {
+                    edt1.setText("");
+                } else {
+                    mValueOne = Float.parseFloat(edt1.getText() + "");
+                    mAddition = true;
+                    edt1.setText(null);
+                }
+            }
+        });
+
+        butSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mValueOne = Float.parseFloat(edt1.getText() + "");
+                mSubtract = true;
+                edt1.setText(null);
+            }
+        });
+
+        butMul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mValueOne = Float.parseFloat(edt1.getText() + "");
+                mMultiplication = true;
+                edt1.setText(null);
+            }
+        });
+
+        butDivision.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mValueOne = Float.parseFloat(edt1.getText() + "");
+                mDivision = true;
+                edt1.setText(null);
+            }
+        });
+
+        butEqual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mValueTwo = Float.parseFloat(edt1.getText() + "");
+
+                if (mAddition == true) {
+
+                    edt1.setText(mValueOne + mValueTwo + "");
+                    mAddition = false;
+                }
+
+
+                if (mSubtract == true) {
+                    edt1.setText(mValueOne - mValueTwo + "");
+                    mSubtract = false;
+                }
+
+                if (mMultiplication == true) {
+                    edt1.setText(mValueOne * mValueTwo + "");
+                    mMultiplication = false;
+                }
+
+                if (mDivision == true) {
+                    edt1.setText(mValueOne / mValueTwo + "");
+                    mDivision = false;
+                }
+            }
+        });
+
+        butC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edt1.setText("");
+            }
+        });
+
+        but10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edt1.setText(edt1.getText() + ".");
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
